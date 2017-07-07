9a538b5be276215179250b563629c2da8941e94e
Andrew Lukefahr <lukefahr@umich.edu>
2017-07-07 12:30:10 -0400

    MAIN:  compiles out asserts for non-debug code

--- firmware/qassert.h
+++ firmware/qassert.h
@@ -1,12 +1,17 @@
 #ifndef __QASSERT_H__
 #define __QASSERT_H__
 
-//quick assert
-#define qassert(condition)                      \
-            do{                                 \
-                while (!(condition)) {}         \
-            } while (false)                     \
+//Uncomment for qassert
+//#define DEBUG
 
-
+#ifdef DEBUG
+    #warning "DEBUG Enabled"
+    #define qassert(condition)                      \
+                do{                                 \
+                    while (!(condition)) {}         \
+                } while(false)   
+#else
+    #define qassert(condition)
+#endif                
 
 #endif
